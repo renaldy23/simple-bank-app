f9125902b761cca0bfeaca645857efdf38fe2ab9
Muhammad Renaldy Ramadhan <renaldyramadhan567@gmail.com>
2025-04-06 22:03:07 +0700

    fix: static files

--- core/urls.py
+++ core/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path("",include("apps.auth.urls")),
     path("",include("apps.profiles.urls")),
     path("",include("apps.transactions.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
